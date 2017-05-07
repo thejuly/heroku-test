4a8256c018b1d2e5fe11048f6f5f83390e99da8a
thejuly <thejuly21574@gmail.com>
2017-05-08 06:58:53 +0700

    test

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import requests
 
 app = Flask(__name__)
 
+# for test route
 @app.route('/')
 def hello():
     return 'Hello World!'
@@ -26,6 +27,42 @@ def profile(user):
 @app.route('/post/<int:post_id>')
 def post(post_id):
     return '<h2>Tuna is good %s</h2>' % post_id
+# end test route
+
+# for line bot
+@app.route('/callback', methods=['POST'])
+def callback():
+    json_line = request.get_json()
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    user = decoded["events"][0]['replyToken']
+    #id=[d['replyToken'] for d in user][0]
+    #print(json_line)
+    print('print User',user)
+    sendText(user,'sendText')
+    return '',200
+
+def sendText(user, text):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    Authorization = 'wbeBaLPb7xIuGymdaHU9yHy300QZ383XYgewhXLSoRe3TnlWo1xQuypNFpis1ExGrSTV1WpmtmQEiaR9tRPQHFUspwI9rVk2Ajfrg1WUwFpV9ewvq/vDx9LItfeNW+9y6Ih/OcwNpJPB/UfE9afIFwdB04t89/1O/w1cDnyilFU='
+ 
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization':Authorization
+    }
+ 
+    data = json.dumps({
+        "replyToken":user,
+        "messages":[{
+            "type":"text",
+            "text":text
+        }]
+    })
+ 
+    r = requests.post(LINE_API, headers=headers, data=data)
+    
+
+# end line bot
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
