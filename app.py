cbffca2d35aef9db5868986a28d80d4e379d3e69
thejuly <thejuly21574@gmail.com>
2017-05-08 12:33:28 +0700

    test

--- app.py
+++ app.py
@@ -1,83 +1,117 @@
-# -*- coding: utf-8 -*-
 import os
 from flask import Flask, request
 import json
 import requests
 
-import re
-import random
-from bs4 import BeautifulSoup
-from collections import defaultdict
-from flask import Flask, request, abort
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-# end import module
 
 app = Flask(__name__)
-line_bot_api = LineBotApi('wbeBaLPb7xIuGymdaHU9yHy300QZ383XYgewhXLSoRe3TnlWo1xQuypNFpis1ExGrSTV1WpmtmQEiaR9tRPQHFUspwI9rVk2Ajfrg1WUwFpV9ewvq/vDx9LItfeNW+9y6Ih/OcwNpJPB/UfE9afIFwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('49a9d31e3b8135ee7f85e6bc78848baa')
-#end Token
+
+# for test route
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+@app.route('/tuna')
+def tuna():
+    return '<h2>Tuna is good</h2>'
+
+@app.route('/profile/<user>')
+def profile(user):
+    return '<h2>Tuna is good %s</h2>' % user
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return '<h2>Tuna is good %s</h2>' % post_id
+
+@app.route('/temp')
+def temp():
+    userThongpoon = 'fdbc6747edd645dc9f77dca0eb120e94'
+    sendMessage = 'Temp report in period'
+    sendText(userThongpoon,sendMessage)
+    return '',200
+
+# end test route
 
 
-
-
-@app.route("/callback", methods=['POST'])
+# for line bot
+@app.route('/callback', methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    json_line = request.get_json()
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    # print("body:",body)
-    app.logger.info("Request body: " + body)
+    #user = user who send message to me
+    #messageGet = message that user send to me
+    #typeGet = type of data that user send to me
+    user = decoded["events"][0]['replyToken']
+    messageGet = decoded["events"][0]['message']['text']
+    typeGet = decoded["events"][0]['message']['type']
+    #print('print User',user)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+    if messageGet == 'Temp':
+        sendMessage = 'report temp'
+        sendText(user,sendMessage)
 
-    return 'OK'
+    if messageGet == 'Rec':
+        sendMessage = 'report rectifier'
+        sendText(user,sendMessage)
 
+    if messageGet == 'Cont':
+        sendMessage = 'report Controller'
+        sendText(user,sendMessage)
 
+    if messageGet == 'Netw':
+        sendMessage = 'report Network'
+        pushText('Auto Thongpoon 0684',sendMessage)
+    
+    #sendText(user,'sendText')
+    #sendText(user,json_line)
+    return '',200
 
+def sendText(user, text):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    Authorization = 'Bearer wbeBaLPb7xIuGymdaHU9yHy300QZ383XYgewhXLSoRe3TnlWo1xQuypNFpis1ExGrSTV1WpmtmQEiaR9tRPQHFUspwI9rVk2Ajfrg1WUwFpV9ewvq/vDx9LItfeNW+9y6Ih/OcwNpJPB/UfE9afIFwdB04t89/1O/w1cDnyilFU='
+ 
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization':Authorization
+    }
+ 
+    data = json.dumps({
+        "replyToken":user,
+        "messages":[{
+            "type":"text",
+            "text":text
+        }]
+    })
+ 
+    r = requests.post(LINE_API, headers=headers, data=data)
+    
 
-
-
-
-
-
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    print("event.reply_token:", event.reply_token)
-    print("event.message.text:", event.message.text)
-    if event.message.text == "aa":
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='your text is aa'))
-        return 0
-
-    if event.message.text == "bb":
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='your text is bb'))
-        return 0
-
-
-    if event.message.text == "cc":
-        line_bot_api.push_message('<Auto Thongpoon 0684>', TextSendMessage(text='Hello World!'))
-        return 0
-
-
-
-
-
-
-
+def pushText(user, text):
+    LINE_API = 'https://api.line.me/v2/bot/message/push'
+    Authorization = 'Bearer wbeBaLPb7xIuGymdaHU9yHy300QZ383XYgewhXLSoRe3TnlWo1xQuypNFpis1ExGrSTV1WpmtmQEiaR9tRPQHFUspwI9rVk2Ajfrg1WUwFpV9ewvq/vDx9LItfeNW+9y6Ih/OcwNpJPB/UfE9afIFwdB04t89/1O/w1cDnyilFU='
+ 
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization':Authorization
+    }
+ 
+    data = json.dumps({
+        "to":user,
+        "messages":[{
+            "type":"text",
+            "text":text
+        }]
+    })
+ 
+    r = requests.post(LINE_API, headers=headers, data=data)
+    
+# end line bot
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
