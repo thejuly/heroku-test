55dd37a353e3c2d34a31d37430d86124c8528f5a
thejuly <thejuly21574@gmail.com>
2017-05-08 10:36:38 +0700

    test

--- app.py
+++ app.py
@@ -1,91 +1,66 @@
+from __future__ import unicode_literals
+
 import os
-from flask import Flask, request
-import json
-import requests
+import sys
+from argparse import ArgumentParser
 
-
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookParser
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
-# for test route
-@app.route('/')
-def hello():
-    return 'Hello World!'
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
 
-@app.route('/tuna')
-def tuna():
-    return '<h2>Tuna is good</h2>'
-
-@app.route('/profile/<user>')
-def profile(user):
-    return '<h2>Tuna is good %s</h2>' % user
-
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    return '<h2>Tuna is good %s</h2>' % post_id
-
-@app.route('/temp')
-def temp():
-    userThongpoon = 'fdbc6747edd645dc9f77dca0eb120e94'
-    sendMessage = 'Temp report in period'
-    sendText(userThongpoon,sendMessage)
-    return '',200
-
-# end test route
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
 
 
-# for line bot
-@app.route('/callback', methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
-    json_line = request.get_json()
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
+    signature = request.headers['X-Line-Signature']
 
-    #user = user who send message to me
-    #messageGet = message that user send to me
-    #typeGet = type of data that user send to me
-    user = decoded["events"][0]['replyToken']
-    messageGet = decoded["events"][0]['message']['text']
-    typeGet = decoded["events"][0]['message']['type']
-    #print('print User',user)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-    if messageGet == 'Temp':
-        sendMessage = 'report temp'
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-    if messageGet == 'Rec':
-        sendMessage = 'report rectifier'
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
 
-    if messageGet == 'Cont':
-        sendMessage = 'report Controller'        
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
-    if messageGet == 'Netw':
-        sendMessage = 'report Network'   
-    
-    #sendText(user,'sendText')
-    sendText(user,json_line)
-    return '',200
+    return 'OK'
 
-def sendText(user, text):
-    LINE_API = 'https://api.line.me/v2/bot/message/reply'
-    Authorization = 'Bearer wbeBaLPb7xIuGymdaHU9yHy300QZ383XYgewhXLSoRe3TnlWo1xQuypNFpis1ExGrSTV1WpmtmQEiaR9tRPQHFUspwI9rVk2Ajfrg1WUwFpV9ewvq/vDx9LItfeNW+9y6Ih/OcwNpJPB/UfE9afIFwdB04t89/1O/w1cDnyilFU='
- 
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization':Authorization
-    }
- 
-    data = json.dumps({
-        "replyToken":user,
-        "messages":[{
-            "type":"text",
-            "text":text
-        }]
-    })
- 
-    r = requests.post(LINE_API, headers=headers, data=data)
-    
 
-# end line bot
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
